d8d29c7c0b7fed4b9088c59b3dd3ebcc00a32428
Alexander Frick <Alex313031@gmail.com>
2026-03-15 05:46:38 -0500

    add clang format files

---
+++ clang_format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copyright (c) 2026 Alex313031.
+
+export HERE=${PWD} &&
+
+export CLANG_FORMAT_FILE=${HERE}/.clang-format &&
+
+# Check header files first
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.h,*.cc}
+
+exit 0
